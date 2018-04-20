23cea152e88641da2389a1ba585cac1ee1177977
mintsuki <xenos1@protonmail.com>
2018-04-20 05:09:00 +0200

    Added C e820 stuff

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include <vga_textmode.h>
 #include <klib.h>
+#include <e820.h>
 
 void kmain(void) {
     init_vga_textmode();
 
     kprint(KPRN_INFO, "uks: Kernel booted");
+
+    init_e820();
 
     for (;;) {
         #asm
